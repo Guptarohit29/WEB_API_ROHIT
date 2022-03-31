da3ff4719637f729ddd28612fbc7656e4886f8c8
Rohit gupta <rohitgupta2490@gmail.com>
2022-03-31 11:06:39 +0530

    WEB_API

---
+++ WEB_API/Controllers/EmpController1.cs
@@ -0,0 +1,47 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WEB_API.db_context;
+using WEB_API.Models;
+
+namespace web_Api_crud.Controllers
+{
+
+    public class EmpController : ApiController
+    {
+        [HttpGet]
+        [Route("emp/getemployee")]
+        // GET api/<controller>
+        public List<MYTABLE1> getname()
+        {
+            List<modelclass> listemp = new List<modelclass>();
+
+            myprojectEntities objlist = new myprojectEntities();
+
+            var res = objlist.MYTABLE1.ToList();
+
+            return res;
+        }
+
+        [HttpPost]
+        [Route("emp/saveEmployee")]
+        public List<string> saveemp()
+        {
+
+            List<string> obj = new List<string>()
+            {
+                "neelam",
+                "rohit",
+                "vikas"
+
+            };
+
+
+            return obj;
+        }
+    }
+}
---
+++ WEB_API/Models/modelclass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WEB_API.Models
+{
+    public class modelclass
+    {
+        public int ID { get; set; }
+        public string NAME { get; set; }
+        public Nullable<int> AGE { get; set; }
+        public Nullable<int> SALARY { get; set; }
+        public string CITY { get; set; }
+    }
+}
